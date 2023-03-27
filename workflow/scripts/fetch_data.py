b2bad9f0a53cecd89811b049ab6e3a21744cae14
Julian Pipart <julian.pipart@fu-berlin.de>
2023-03-27 08:45:13 +0200

    fix when no sequence is found

--- workflow/scripts/fetch_data.py
+++ workflow/scripts/fetch_data.py
@@ -6,23 +6,32 @@ mapped_taxids = snakemake.input[0]
 concat_fasta = snakemake.output[0]
 number_taxids = snakemake.params[0]
 stderr_log = snakemake.log[0]
+stdout_log = snakemake.log[1]
 
 with open(stderr_log, "w") as sys.stderr:
+    with open(stdout_log, "w") as sys.stdout:
 
-    taxid_df = pd.read_csv(mapped_taxids, sep="\t", header=0, index_col=0)
-    taxids = taxid_df["taxid"].value_counts()[:number_taxids]
-    taxids = taxids.index.to_list()
+        taxid_df = pd.read_csv(mapped_taxids, sep="\t", header=0, index_col=0)
+        taxids = taxid_df["taxid"].value_counts()[:number_taxids]
+        taxids = taxids.index.to_list()
 
-    genome_ids = []
-    for taxid in taxids:
-        q = f"refseq[filter] AND txid{taxid}[ORGN]"
-        handle = Entrez.esearch(db="nucleotide", term=q, retmax=1000)
-        genome_ids.append(int(handle.readlines()[3][4:-6]))
-        handle.close()
+        genome_ids = []
+        for taxid in taxids:
+            q = f"refseq[filter] AND txid{taxid}[ORGN]"
+            handle = Entrez.esearch(db="nucleotide", term=q, retmax=1000)
+            complete_handle = handle.readlines()
+            if "<Count>0" in str(complete_handle[2]):
+                print(f"No genome found for taxid {taxid}")
+                continue
+            genome_id = int(complete_handle[3][4:-6])
+            genome_ids.append(genome_id)
+            handle.close()
 
-    sequences = Entrez.efetch(db="nucleotide", id=genome_ids, rettype="fasta", retmax=1000).read()
-    with open(concat_fasta, "w") as f:
-        f.write(sequences)
+        sequences = Entrez.efetch(db="nucleotide", id=genome_ids, rettype="fasta", retmax=1000).read()
+        num_seq = sequences.count(">")
+        print(f"There were {num_seq} genome sequences found.")
+        with open(concat_fasta, "w") as f:
+            f.write(sequences)
 
 
 
