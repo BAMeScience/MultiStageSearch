566963136370b52a93d5b5b97a5fb4971749d6ca
Julian Pipart <julian.pipart@fu-berlin.de>
2023-03-27 08:44:37 +0200

    add more params for sixpack + logs

--- workflow/rules/genome2proteome.smk
+++ workflow/rules/genome2proteome.smk
@@ -5,7 +5,9 @@ rule genome2Proteome:
         touch(RESULT_DIR / "{sample}/sixpack/genome2Proteome.done"),
         sixpack_temp = RESULT_DIR / "{sample}/sixpack/sixpack_temp.txt"
     params:
-        sample_path = str(RESULT_DIR / "{sample}")
+        sample_path = str(RESULT_DIR / "{sample}"),
+        orfminsize = config["sixpack"]["orfminsize"],
+        additional_params = config["sixpack"]["additional_parameters"],
     conda:
         "../envs/sixpack.yml"
     threads: 1
@@ -16,10 +18,10 @@ rule concatProteomes:
     input:
         sixpack_done = RESULT_DIR / "{sample}/sixpack/genome2Proteome.done"
     output:
-        concat_proteomes = RESULT_DIR / "{sample}/Database/concat_proteomes.fasta"
+        concat_proteomes = RESULT_DIR / "{sample}/Database/proteomes.fasta"
     params:
         res_dir = RESULT_DIR,
-        sample_name = "{sample}"
+        sample_name = "{sample}",
     conda:
         "../envs/base_python.yml"
     threads: 1
@@ -28,9 +30,9 @@ rule concatProteomes:
 
 rule AddDecoysProteome:
     input: 
-        concat_proteomes = RESULT_DIR / "{sample}/Database/concat_proteomes.fasta"
+        proteomes = RESULT_DIR / "{sample}/Database/proteomes.fasta"
     output: 
-        touch(RESULT_DIR / "{sample}/Database/AddDecoysProteomes.done")
+        proteomes_decoy = RESULT_DIR / "{sample}/Database/proteomes_concatenated_target_decoy.fasta"
     log:
         stderr_log = RESULT_DIR / "logs/genome2proteome/proteomeDB/{sample}/stderr.log",
         stdout_log = RESULT_DIR / "logs/genome2proteome/proteomeDB/{sample}/stdout.log" 
@@ -38,4 +40,4 @@ rule AddDecoysProteome:
         "../envs/host_filtering.yml"
     threads: 1
     shell: 
-        "searchgui eu.isas.searchgui.cmd.FastaCLI -in {input.concat_proteomes} -decoy > {log.stdout_log} 2> {log.stderr_log}"
+        "searchgui eu.isas.searchgui.cmd.FastaCLI -in {input.proteomes} -decoy > {log.stdout_log} 2> {log.stderr_log}"
--- workflow/scripts/genome2proteome.py
+++ workflow/scripts/genome2proteome.py
@@ -1,16 +1,12 @@
 import subprocess
 import time
 
-# concat_fasta = "/home/jpipart/project/MultiStageSearch/Database/concat_taxid_genomes.fasta"
-# sixpack_temp = "sixpack_temp.txt"
-
-# base_path = "results/PXD003013_Cowpox_BR/sixpack"
-# sample = ""
-
 concat_fasta = snakemake.input[0]
 sixpack_temp = snakemake.output[1]
 
 sample_path = snakemake.params[0] + "/sixpack/"
+orfminsize = snakemake.params[1]
+additional_sixpack_params = snakemake.params[2]
 
 
 with open(concat_fasta, "r") as fasta:
@@ -20,19 +16,39 @@ with open(concat_fasta, "r") as fasta:
 
 with open(sixpack_temp, "w") as f:
     for i, sequence in enumerate(sequences[:-1]):
-        process = subprocess.Popen(
-            [
-                "sixpack",
-                "-sequence",
-                concat_fasta,
-                "-outfile",
-                f"{sample_path}{sequence[1:12]}.orfs",
-                "-outseq",
-                f"{sample_path}{sequence[1:12]}.fasta",
-            ],
-            stdout=f,
-            stderr=subprocess.STDOUT
-        )
+        if additional_sixpack_params:
+            process = subprocess.Popen(
+                [
+                    "sixpack",
+                    "-sequence",
+                    concat_fasta,
+                    "-outfile",
+                    f"{sample_path}{sequence[1:12]}.orfs",
+                    "-outseq",
+                    f"{sample_path}{sequence[1:12]}.fasta",
+                    "-orfminsize",
+                    f"{orfminsize}",
+                    f"{additional_sixpack_params}"
+                ],
+                stdout=f,
+                stderr=subprocess.STDOUT
+            )
+        else:
+            process = subprocess.Popen(
+                [
+                    "sixpack",
+                    "-sequence",
+                    concat_fasta,
+                    "-outfile",
+                    f"{sample_path}{sequence[1:12]}.orfs",
+                    "-outseq",
+                    f"{sample_path}{sequence[1:12]}.fasta",
+                    "-orfminsize",
+                    f"{orfminsize}",
+                ],
+                stdout=f,
+                stderr=subprocess.STDOUT
+            )
         with open(concat_fasta, "w") as fasta:
             for seq in sequences[i:]:
                 # print(seq[:20])
