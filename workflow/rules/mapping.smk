c61d83a1ad05ee66c0dd4598917f9bd8d2173a3a
Julian Pipart <julian.pipart@fu-berlin.de>
2023-03-29 09:16:27 +0200

    add scoring and limit of top scoring taxids

--- workflow/rules/mapping.smk
+++ workflow/rules/mapping.smk
@@ -1,14 +1,15 @@
-rule getTargets:
+rule mapTaxIDs:
     input:
         PSM_Report = RESULT_DIR / "{sample}/FirstSearch/ref_Default_PSM_Report.txt",
         taxIDs = RESULT_DIR / "taxidMapping/protacc2taxids_virus.txt"
     output:
-        RESULT_DIR / "{sample}/taxids/mapped_taxids.tsv",
+        all_mapped_taxIDs = RESULT_DIR / "{sample}/taxids/mapped_taxids.tsv",
+        top_scoring_taxIDs = RESULT_DIR / "{sample}/taxids/top_scoring_taxids.tsv",
     log:
         stderr_log = RESULT_DIR / "logs/taxids/getTargets/{sample}/stderr.log",
         stdout_log = RESULT_DIR / "logs/taxids/getTargets/{sample}/stdout.log"
     params:
-        query= RESULT_DIR / "{sample}/ref_query_accessions.txt",
+        number_taxids = config["mapping"]["number_of_taxids"]
     conda: 
         "../envs/mapping.yml"
     script: 
--- workflow/scripts/taxIDMapping.py
+++ workflow/scripts/taxIDMapping.py
@@ -1,19 +1,51 @@
 import pandas as pd
 
-raw_report = snakemake.input[0]
-taxids = snakemake.input[1]
-out_file = snakemake.output[0]
 
-raw_report_df = pd.read_csv(raw_report, sep="\t", on_bad_lines="skip", usecols=["Protein(s)"])
-id_df = pd.read_csv(taxids, header=None, names=["protein", "taxid"], sep="\t")
+def mapTaxIDs(in_file, taxids, out_file):
+    in_file = pd.read_csv(in_file, sep="\t", on_bad_lines="skip", usecols=["Protein(s)"])
+    id_df = pd.read_csv(taxids, header=None, names=["protein", "taxid"], sep="\t")
 
-raw_report_df.columns = ["accession"]
-raw_report_df["weight"] = 1 / (raw_report_df.accession.str.count(",") + 1)
-raw_report_df["accession"] = raw_report_df.accession.apply(lambda x: x.split(","))
-raw_report_df["psmid"] = raw_report_df.index + 1
-report_df = raw_report_df.explode("accession", ignore_index=True)
+    in_file.columns = ["accession"]
+    in_file["weight"] = 1 / (in_file.accession.str.count(",") + 1)
+    in_file["accession"] = in_file.accession.apply(lambda x: x.split(","))
+    in_file["psmid"] = in_file.index + 1
+    report_df = in_file.explode("accession", ignore_index=True)
 
-merged_df = pd.merge(report_df, id_df, how="inner", left_on="accession", right_on="protein")
-merged_df = merged_df.drop("protein", axis=1)
+    merged_df = pd.merge(report_df, id_df, how="inner", left_on="accession", right_on="protein")
+    merged_df = merged_df.drop("protein", axis=1)
 
-merged_df.to_csv(out_file, sep="\t", header=["accession", "weight", "psmid", "taxid"])
+    merged_df.to_csv(out_file, sep="\t", header=["accession", "weight", "psmid", "taxid"])
+    return merged_df
+
+#TODO Param um scoring mit mean, median oder max number zu machen
+def score(df, num, out_file):
+    """
+    Score taxids according to their confidence and select the ones which are top scoring.
+    :param df: df, contains protein 2 taxid mapping and the respective weights
+    :param taxids: lst, mapped taxids
+    :param output_path: str, output path
+    :param subset: int, top <subset> scoring taxids
+    """
+    #df['taxid'] = taxids
+    df_score = df.groupby('taxid')['weight'].sum().reset_index()
+    df_score = df_score.sort_values(by=['weight'], ascending=False)
+    df_score = df_score[:num]
+    df_score.to_csv(out_file, index=False, sep="\t")
+    #threshold = df_score.weight.mean() # diff to pepgm
+
+    #top_scoring_df = df_score.loc[df_score["weight"] >= threshold]
+    #top_scoring_df.to_csv(out_file, index=False, sep="\t")
+
+
+def main():
+    raw_report = snakemake.input[0]
+    taxids = snakemake.input[1]
+    mapped_taxids = snakemake.output[0]
+    top_scoring = snakemake.output[1]
+    number_taxids = snakemake.params[0]
+
+    merged_df = mapTaxIDs(in_file=raw_report, taxids=taxids, out_file=mapped_taxids)
+    score(df=merged_df, num=number_taxids, out_file=top_scoring)
+
+if __name__ == "__main__":
+    main()
